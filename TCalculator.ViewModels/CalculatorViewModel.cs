086b7e58f72943d4c3d6acf992054541c9a240b9
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2025-01-20 11:19:16 +0200

    Add project files.

---
+++ TCalculator.ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace TCalculator.ViewModels;
+
+public partial class CalculatorViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private decimal firstOperand;
+
+    [ObservableProperty]
+    private decimal secondOperand;
+
+    [ObservableProperty]
+    private decimal result;
+
+    [RelayCommand]
+    private void SumOperands() => SynchronizationContext.Current?.Send(_ => Result = FirstOperand + SecondOperand, null);
+
+    [RelayCommand]
+    private void SubtractOperands() => SynchronizationContext.Current?.Send(_ => Result = FirstOperand - SecondOperand, null);
+
+    [RelayCommand]
+    private void MultiplyOperands() => SynchronizationContext.Current?.Send(_ => Result = FirstOperand * SecondOperand, null);
+
+    [RelayCommand]
+    private void DivideOperands() => SynchronizationContext.Current?.Send(_ => Result = FirstOperand / SecondOperand, null);
+}
